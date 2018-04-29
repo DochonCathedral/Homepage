44842fff5bf683bd8e9137df84cc90e48711faa0
Varian Lee <varian.k.lee@gmail.com>
2018-04-29 12:07:32 +0900

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $MY_BEARER" --header "Dropbox-API-Arg: {\"path\": \"/Homepage/app.jar\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" --header "Content-Type: application/octet-stream" --data-binary ./build/libs/homepage-0.0.1-SNAPSHOT.jar
+curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer $MY_BEARER" --header "Dropbox-API-Arg: {\"path\": \"/Homepage/app.jar\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" --header "Content-Type: application/octet-stream" --data-binary @./build/libs/homepage-0.0.1-SNAPSHOT.jar
