d3635d746b7fc32b2bddf02cefdba58af4831ff2
ysg03 <ysg03@DESKTOP-O64S3BK>
2018-04-29 11:38:35 +0900

    vue.js 개발환경 구성

--- src/main/java/ga/dochon/homepage/controller/TestController.java
+++ src/main/java/ga/dochon/homepage/controller/TestController.java
@@ -1,11 +1,16 @@
 package ga.dochon.homepage.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import ga.dochon.homepage.entity.Organization;
 import ga.dochon.homepage.service.OrganizationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class TestController {
@@ -13,13 +18,16 @@ public class TestController {
     OrganizationService organizationService;
 
     @RequestMapping("/")
-    public String index() {
-        return "Dochon!!!";
+    public String index(Model model) {
+    	model.addAttribute("name", "우와~");
+        return "index";
     }
 
     @RequestMapping(value = "/organization/{idOrganization}", method = RequestMethod.GET)
     public Organization getOrganization(@PathVariable int idOrganization) {
-        return organizationService.getOrganization(idOrganization);
+    	Organization a = organizationService.getOrganization(idOrganization);
+        
+    	return a;
         //return organization.getName() + " / " + organization.getDescription(); // 이렇게 안해도 나와야 되는데 으으으으으으으ㅡ으 몰라 내일
         // 안되는 이유는 자바 10 때문이었음. 스벌.
     }
